e6194686986b5e5e05fb73a2699df5fa7aff7403
Jia Chen <github@jiachen.app>
2025-04-11 16:18:35 +0800

    Initial Commit

---
+++ Capitalist Clicker/Capitalist_ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capitalist_ClickerApp.swift
+//  Capitalist Clicker
+//
+//  Created by Jia Chen Yee on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Capitalist_ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Capitalist Clicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Capitalist Clicker
+//
+//  Created by Jia Chen Yee on 4/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
